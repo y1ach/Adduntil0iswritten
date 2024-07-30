ef21cfb4dcc05cabdae86a861a84f81d60109417
Yach <135635030+y1ach@users.noreply.github.com>
2024-07-30 22:17:50 +0300

    Add files via upload

---
+++ Ödev 1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ödev_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //double toplam = 0;
+            //double sayi = 1;
+            //double sayisayisi = 0;
+            //Console.WriteLine("Pozitif sayılar giriniz (durdurmak için 0 giriniz):");
+
+            //while (true)
+            //{
+            //    Console.WriteLine("Sayı girin:");
+            //    sayi = Convert.ToDouble(Console.ReadLine());
+
+            //    if (sayi >= 0)
+            //    {
+            //        if (sayi == 0)
+            //        {
+            //            break;
+            //        }
+            //        else
+            //        {
+            //            toplam = toplam + sayi;
+            //            sayisayisi++;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Girdiğiniz sayı pozitif değil tekrar deneyin");
+            //    }
+            //}
+            //double ortalama = toplam / sayisayisi;
+            //Console.WriteLine("Toplam: " + toplam);
+            //Console.WriteLine("Ortalama :" + ortalama);
+
+            //Console.ReadLine();
+
+
+        }
+    }
+}
